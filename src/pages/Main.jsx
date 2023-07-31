2d93164b07b39cc42047fd04a542c19a366a1b50
yoona1110 <gjdbsdk18@icloud.com>
2023-07-31 20:45:48 +0900

    Feat: main page 상단만 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,24 +1,79 @@
+import { useState, useCallback } from "react";
 import styled from "styled-components";
 
 const PageComponent = styled.div`
-    display: flex;
+    display: block;
+    flex-direction: column;
+    justify-content: center;
 `;
 
-const Category = styled.div`
-    width: 55px;
-    height: 55px;
+const Image = styled.img`
+    width: 100%;
+    height: 18vh;
+    background: #EEF1F4;
+    margin-top: 10px;
+    border: none;
+`;
+
+const Menu = styled.div`
+    width: 60px;
+    height: 60px;
     font-size: 11px;
     color: #667080;
     background-color: #EEF1F4;
-    text-align: center;
+    margin: 15px 15px 0 0;
+    padding-bottom: 8px;
+    justify-content: center;
     align-items: end;
     display: flex;
-    flex-direction: row;
-    margin-left: 10px;
+    float: left;
 `;
+
+// 카테고리 배열 생성
+const Category = [
+    {
+        name: 'snack',
+        text: "간식"
+    },
+    {
+        name: 'beauty',
+        text: "미용"
+    },
+    {
+        name: 'accessories',
+        text: "악세사리"
+    },
+    {
+        name: 'nutritional',
+        text: "영양제"
+    },
+    {
+        name: 'toys',
+        text: "장난감"
+    },
+    {
+        name: 'toilet',
+        text: "배변용품"
+    }
+]
+
 const Main = () => {
+    const [category, setCategory] = useState('home');
+    const onSelect = useCallback(
+        Menu => setCategory(Menu), []
+    );
+
     return (
         <PageComponent>
+            <Image/>
+            {Category.map((cate) => 
+                <Menu
+                    key={cate.name}
+                    onClick={() => onSelect(cate.name)}
+                >
+                    {cate.text}
+                </Menu>
+            )}
         </PageComponent>
     );
 }
