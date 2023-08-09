87ce02f601b7bc5a1e16890fb1d4b768e24b6f18
osohyun0224 <osohyun0224@naver.com>
2023-08-09 22:46:02 +0900

    Feat: 회원가입 완료 페이지 디자인 수정 완료

--- src/pages/LoginSignup/SignCompelete.jsx
+++ src/pages/LoginSignup/SignCompelete.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { userLogin } from "../../librarys/login-api";
 
 const PageContainer = styled.div`
   display: flex;
@@ -19,61 +21,67 @@ const SignupTitle = styled.h3`
   font-family: "Nanum Gothic";
   font-size: 24px;
   font-weight: 700;
-  margin-top: 80px;
-  margin-left: -20px;
-  color: #667080;
+  line-height: 22px;
+  letter-spacing: 0.07em;
+  text-align: center;
+  margin-top: 239px;
+  margin-left: 120px;
 `;
 
-const SignupText = styled.p`
+const HeaderName = styled.p`
   font-family: "Nanum Gothic";
-  font-size: 14px;
-  font-weight: 500;
-  text-align: left;
-  margin-top: 10px;
-  margin-left: -20px;
-  color: #667080;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 22px;
+  letter-spacing: 0.07em;
+  text-align: center;
+  margin-top: 14px;
+  margin-left: 130px;
 `;
 
 const BottomBox = styled(Link)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 84px;
-  background-color: #667080;
+  border-radius: 5px;
+  width: 275px;
+  height: 50px;
+  background-color: #d94a56;
+  font-family: "Nanum Gothic";
   color: white;
   text-align: center;
   position: absolute;
   bottom: 0;
   text-decoration: none;
-  margin-left: -50px;
-`;
-
-const SkipButton = styled.p`
-  font-family: "Nanum Gothic";
-  position: absolute;
-  right: 20px;
-  top: 20px;
-  color: #667080;
-  cursor: pointer;
+  margin-left: 60px;
+  margin-bottom: 120px;
 `;
 
 function SignCompelete() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const loginUser = async () => {
+      const loginInfo = await userLogin("likelion1@example.com", "qwerty123");
+      setUser(loginInfo);
+    };
+
+    loginUser();
+  }, []);
   return (
     <PageContainer>
       <Link to="/"></Link>
-      <SignupTitle>가입을 환영합니다!</SignupTitle>
-      <SignupText>
-        지금 바로
-        <br /> 반려동물을 등록해보세요
-      </SignupText>
-      <BottomBox to="/">추가하기</BottomBox>
-      <Link to="/main">
-        <SkipButton>건너뛰기</SkipButton>
-      </Link>
+      <SignupTitle>환영합니다!</SignupTitle>
+      {user ? (
+        <>
+          <HeaderName>{user.name} 님! </HeaderName>
+        </>
+      ) : (
+        <HeaderName>Loading...</HeaderName>
+      )}
+      <BottomBox to="/main">메인페이지로 가기</BottomBox>
     </PageContainer>
   );
 }
 
-//BottomBox to="/"에 누가 반려동물 추가하는 페이지 구현하면 연결 좀 해주세요 - 소현
 export default SignCompelete;
