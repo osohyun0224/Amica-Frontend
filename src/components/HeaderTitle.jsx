4e8e11ec2756cde45c98bd81a7f2ddbf4d70d9f1
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-08-14 00:02:26 +0900

    Feat: 공용 컴포넌트 HeaderTitle 추가

---
+++ src/components/HeaderTitle.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import BackButton from "../assets/images/getback.png";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  padding: 24px 12px;
+  display: flex;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 48px;
+  height: 48px;
+  object-fit: contain;
+  cursor: pointer;
+`;
+
+const Title = styled.p`
+  margin-right: 48px;
+  flex-grow: 1;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+const HeaderTitle = ({ url = "/main", title }) => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Image onClick={() => navigate(url)} src={BackButton} />
+      <Title>{title}</Title>
+    </Container>
+  );
+};
+
+HeaderTitle.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+};
+
+export default HeaderTitle;
