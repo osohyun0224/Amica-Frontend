604a57862b1e2003f8b07d2f9f91678bd182bc7d
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-08-13 23:35:58 +0900

    Feat: 권한에 따라서 redirect url 변경

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,24 @@
 import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
-import { selectIsLoggedIn } from "../redux/userSlice";
+import { selectIsAdmin, selectIsLoggedIn } from "../redux/userSlice";
 import { useSelector } from "react-redux";
 
-function ProtectedRoute({ children, to = null }) {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = true;
+function ProtectedRoute({ children, to = null, role = 1 }) {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isAdmin = useSelector(selectIsAdmin);
+  const userRole = isLoggedIn ? (isAdmin ? 2 : 1) : 0;
   const pathname = useLocation().pathname;
   const redirect = to || pathname;
-  const url = "/login" + (redirect ? "?redirect=" + redirect : "");
 
-  return isLoggedIn ? <>{children}</> : <Navigate to={url} />;
+  const url = userRole >= 1 ? "/main" : "/login?redirect=" + redirect;
+
+  return userRole >= role ? <>{children}</> : <Navigate to={url} />;
 }
 
 ProtectedRoute.propTypes = {
   children: PropTypes.node,
-  redirect: PropTypes.string,
+  to: PropTypes.string,
+  role: PropTypes.number,
 };
 
 export default ProtectedRoute;
