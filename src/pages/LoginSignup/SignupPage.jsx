47f774d07f357f301212e84b892666c29b6714d0
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-08-16 00:19:27 +0900

    Feat: 회원가입 페이지 리워크 (2)

--- src/pages/LoginSignup/SignupPage.jsx
+++ src/pages/LoginSignup/SignupPage.jsx
@@ -1,235 +1,91 @@
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import BackButton from "../../assets/images/getback.png";
+
 import TermsAgreement from "../../components/login/TermsAgreement.jsx";
 import EmailInput from "../../components/login/EmaiInput.jsx";
 import PasswordInputForm from "../../components/login/PasswordInputForm.jsx";
 import UserInfo from "../../components/login/UserInfo.jsx";
+import HeaderTitle from "../../components/HeaderTitle.jsx";
 
-const PageContainer = styled.div`
+import { DispatchContext, StateContext } from "../../librarys/context";
+import { intialSignupState, signupReducer } from "../../reducer/signUp";
+
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  background-color: white;
   min-height: calc(var(--vh) * 100);
-  margin: 0 auto;
-  max-width: 1000px;
-  padding-top: 30px;
-  padding-left: 50px;
-  position: relative;
-  z-index: 1;
 `;
 
-const Header = styled.header`
-  width: 111%;
-  height: 50px;
-  background: #eef1f4;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  margin-top: -31px;
-  margin-left: -50px;
-`;
-
-const HeaderTitle = styled.h1`
-  font-family: "Nanum Gothic";
-  font-size: 20px;
+const Label = styled.p`
+  margin: 8px 24px;
+  margin-top: 12px;
+  font-size: 16px;
   font-weight: 700;
-  line-height: 30px;
-  letter-spacing: -0.02em;
-  text-align: center;
-  color: #151515;
+  color: rgba(21, 21, 21, 1);
 `;
 
-const BackButtonImage = styled.img`
-  position: absolute;
-  left: 15px;
-  margin-top: -20px;
-  cursor: pointer;
+const Spacer = styled.div`
+  flex-grow: 1;
 `;
 
-const BottomBox = styled(Link)`
+const BottomBox = styled.button`
+  padding: 24px;
+  border: none;
+
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 84px;
+
   background-color: #d94a56;
   color: white;
   text-align: center;
-  position: absolute;
-  bottom: 0;
-  text-decoration: none;
-  margin-left: -50px;
+
   opacity: ${(props) => (props.active ? "1" : "0.5")};
-  pointer-events: ${(props) => (props.active ? "auto" : "none")};
+  cursor: ${(props) => (props.active ? "pointer" : "auto")};
+
+  transition: opacity 0.2s;
 `;
 
 function SignupPage() {
-  const [agree, setAgree] = useState(false);
-  const [agreeTerms, setAgreeTerms] = useState(false);
-  const [agreePrivacy, setAgreePrivacy] = useState(false);
-  const [agreeMarketing, setAgreeMarketing] = useState(false);
-  const [email, setEmail] = useState("");
-  const [invalidEmail, setInvalidEmail] = useState(false);
-  const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
-  const [isValid, setIsValid] = useState(false);
-  const [lengthRequirement, setLengthRequirement] = useState(false);
-  const [letterRequirement, setLetterRequirement] = useState(false);
-  const [numberRequirement, setNumberRequirement] = useState(false);
-  const [isPasswordMatch, setIsPasswordMatch] = useState(null);
-  const [username, setUsername] = useState("");
-  const [userphone, setUserPhone] = useState("");
-
-  const handleUserPhoneChange = (e) => {
-    const userphoneInput = e.target.value;
-    setUserPhone(userphoneInput);
-  };
-
-  const handleUsernameChange = (e) => {
-    const usernameInput = e.target.value;
-    setUsername(usernameInput);
-  };
-
-  const handlePasswordChange = (e) => {
-    const passwordValue = e.target.value;
-    setPassword(passwordValue);
-    setLengthRequirement(passwordValue.length >= 8);
-    setLetterRequirement(/[a-zA-Z]/.test(passwordValue));
-    setNumberRequirement(/\d/.test(passwordValue));
-  };
-
-  const handlePasswordCheckChange = (e) => {
-    setPasswordCheck(e.target.value);
-  };
-
-  useEffect(() => {
-    const checkPassword = (password, passwordCheck) => {
-      const matchRequirement = password === passwordCheck;
-      setIsValid(
-        lengthRequirement &&
-          letterRequirement &&
-          numberRequirement &&
-          matchRequirement,
-      );
-      setIsPasswordMatch(password && passwordCheck ? matchRequirement : null);
-    };
-
-    checkPassword(password, passwordCheck);
-  }, [
-    password,
-    passwordCheck,
-    lengthRequirement,
-    letterRequirement,
-    numberRequirement,
-  ]);
-
-  const handleEmailChange = (e) => {
-    const emailInput = e.target.value;
-    setEmail(emailInput);
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(emailInput)) {
-      setInvalidEmail(true);
-    } else {
-      setInvalidEmail(false);
-    }
-  };
-
-  const toggleCheckboxTerms = () => {
-    const newAgreeTermsState = !agreeTerms;
-    setAgreeTerms(newAgreeTermsState);
-    if (!newAgreeTermsState) setAgree(false);
-  };
-
-  const toggleCheckboxPrivacy = () => {
-    const newAgreePrivacyState = !agreePrivacy;
-    setAgreePrivacy(newAgreePrivacyState);
-    if (!newAgreePrivacyState) setAgree(false);
-  };
-
-  const toggleCheckboxMarketing = () => {
-    const newAgreeMarketingState = !agreeMarketing;
-    setAgreeMarketing(newAgreeMarketingState);
-    if (!newAgreeMarketingState) setAgree(false);
-  };
-
   const navigate = useNavigate();
+  const [state, dispatch] = useReducer(signupReducer, intialSignupState);
+  const {
+    agreement,
+    marketing,
+    emailCheck,
+    passwordCheck,
+    nameCheck,
+    phoneCheck,
+  } = state;
 
-  const toggleCheckbox = () => {
-    const newAgreeState = !agree;
-    setAgree(newAgreeState);
-    setAgreeTerms(newAgreeState);
-    setAgreePrivacy(newAgreeState);
-    setAgreeMarketing(newAgreeState);
-  };
+  const isComplete =
+    agreement && emailCheck && passwordCheck && nameCheck && phoneCheck;
 
-  const handleNextClick = (e) => {
-    e.preventDefault();
-    if (agreeTerms && agreePrivacy) {
+  function onClick() {
+    if (isComplete) {
       navigate("/compelete");
     }
-  };
+  }
 
   return (
-    <PageContainer>
-      <Header>
-        <Link to="/">
-          <BackButtonImage src={BackButton} alt="Back" />
-        </Link>
-        <HeaderTitle>회원가입</HeaderTitle>
-      </Header>
-
-      <TermsAgreement
-        {...{
-          agree,
-          agreeTerms,
-          agreePrivacy,
-          agreeMarketing,
-          toggleCheckbox,
-          toggleCheckboxTerms,
-          toggleCheckboxPrivacy,
-          toggleCheckboxMarketing,
-        }}
-      />
-
-      <EmailInput
-        email={email}
-        invalidEmail={invalidEmail}
-        handleEmailChange={handleEmailChange}
-      />
-      <PasswordInputForm
-        {...{
-          password,
-          passwordCheck,
-          handlePasswordChange,
-          handlePasswordCheckChange,
-          lengthRequirement,
-          letterRequirement,
-          numberRequirement,
-          isPasswordMatch,
-        }}
-      />
-      <UserInfo
-        {...{
-          username,
-          userphone,
-          handleUsernameChange,
-          handleUserPhoneChange,
-        }}
-      />
-
-      <BottomBox
-        to="/compelete"
-        onClick={handleNextClick}
-        active={agreeTerms && agreePrivacy && isValid ? 1 : 0}
-      >
-        가입하기
-      </BottomBox>
-    </PageContainer>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <Container>
+          <HeaderTitle url="/" title="회원가입" />
+          <TermsAgreement />
+          <Label>이메일</Label>
+          <EmailInput />
+          <Label>비밀번호</Label>
+          <PasswordInputForm />
+          <UserInfo />
+          <Spacer />
+          <BottomBox to="/compelete" onClick={onClick} active={isComplete}>
+            가입하기
+          </BottomBox>
+        </Container>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   );
 }
 export default SignupPage;
