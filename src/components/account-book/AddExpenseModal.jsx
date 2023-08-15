eaf57574bf68a10bc47b1d80b1775fde3c1cb76e
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-08-15 20:29:45 +0900

    Refactor: 가계부 소비내역 modal 리팩토링

--- src/components/account-book/AddExpenseModal.jsx
+++ src/components/account-book/AddExpenseModal.jsx
@@ -2,75 +2,11 @@ import PropTypes from "prop-types";
 import { styled } from "styled-components";
 
 import Modal from "../Modal.jsx";
-import Select from "../Select.jsx";
+import ExpenseModalForm from "./ExpenseModalForm.jsx";
 
 import { hide } from "../../redux/modalSlice.js";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import { categorys } from "../../librarys/data.js";
-
-const Container = styled.div`
-  margin: 16px;
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-
-const Title = styled.p`
-  width: 100%;
-  margin-bottom: 24px;
-  font-weight: 600;
-  font-size: 20px;
-  text-align: center;
-`;
-
-const Text = styled.p`
-  font-weight: 600;
-  font-size: 14px;
-`;
-
-const Textbox = styled.input`
-  padding: 8px;
-  border: none;
-  border-radius: 6px;
-  font-size: 16px;
-  background-color: rgba(248, 248, 248, 1);
-
-  &::placeholder {
-    color: rgba(21, 21, 21, 0.3);
-  }
-`;
-
-const TextboxContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr 16px);
-  align-items: center;
-  gap: 12px;
-
-  & > input {
-    width: 100%;
-    text-align: right;
-  }
-
-  & > p {
-    width: 100%;
-    text-align: center;
-  }
-`;
-
-const PriceContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-
-  & > input {
-    text-align: right;
-    flex-grow: 1;
-    flex-shrink: 1;
-  }
-`;
-
-const Dropdown = styled(Select)``;
 
 const CoverButton = styled.button`
   width: 100%;
@@ -89,43 +25,15 @@ const CoverButton = styled.button`
   cursor: pointer;
 `;
 
-const intialList = categorys.map(({ id, title }, index) => ({
-  id,
-  name: title,
-  default: index === 0,
-}));
-
 const id = "add_expense";
 
-const AddExpenseModal = ({}) => {
+const AddExpenseModal = () => {
   const dispatch = useDispatch();
-
-  const [list, setList] = useState(intialList);
+  const [data, setData] = useState(null);
 
   return (
     <Modal id={id}>
-      <Container>
-        <Title>추가하기</Title>
-        <Text>날짜</Text>
-        <TextboxContainer>
-          <Textbox type="text" placeholder="2023" maxLength="4" />
-          <Text>년</Text>
-          <Textbox type="text" placeholder="8" maxLength="2" />
-          <Text>월</Text>
-          <Textbox type="text" placeholder="13" maxLength="2" />
-          <Text>일</Text>
-        </TextboxContainer>
-
-        <Text>금액</Text>
-        <PriceContainer>
-          <Textbox type="text" placeholder="14000" />
-          <Text>원</Text>
-        </PriceContainer>
-        <Text>이름</Text>
-        <Textbox type="text" placeholder="달이 장난감" />
-        <Text>카테고리</Text>
-        <Dropdown list={list} outline={true} />
-      </Container>
+      <ExpenseModalForm onChange={setData} />
       <CoverButton onClick={() => dispatch(hide(id))}>확인</CoverButton>
     </Modal>
   );
---
+++ src/components/account-book/ExpenseModalForm.jsx
@@ -0,0 +1,135 @@
+import PropTypes from "prop-types";
+import { styled } from "styled-components";
+
+import { useState, useEffect } from "react";
+
+import Select from "../Select";
+
+import { categorys } from "../../librarys/data";
+import { filterNumber } from "../../librarys/util";
+
+import dayjs from "dayjs";
+
+const Container = styled.div`
+  margin: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Title = styled.p`
+  width: 100%;
+  margin-bottom: 24px;
+  font-weight: 600;
+  font-size: 20px;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Textbox = styled.input`
+  padding: 8px;
+  border: none;
+  border-radius: 6px;
+  font-size: 16px;
+  background-color: rgba(248, 248, 248, 1);
+
+  &::placeholder {
+    color: rgba(21, 21, 21, 0.3);
+  }
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  & > input {
+    text-align: right;
+    flex-grow: 1;
+    flex-shrink: 1;
+  }
+`;
+
+const dateFormat = "YYYY-MM-DD";
+
+const categoryList = categorys.map((item) => ({
+  id: item.id,
+  name: item.title,
+}));
+
+const ExpenseModalForm = ({ data, onChange }) => {
+  const [dateState, setDate] = useState(dayjs().format(dateFormat));
+  const [priceState, setPrice] = useState(0);
+  const [nameState, setName] = useState("");
+  const [categoryState, setCategory] = useState(categoryList[0].id);
+
+  useEffect(() => {
+    if (data) {
+      setDate(dayjs(data.date).format(dateFormat));
+      setPrice(data.price);
+      setName(data.name);
+      setCategory(data.category);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    onChange({
+      date: dateState,
+      price: priceState,
+      name: nameState,
+      category: categoryState,
+    });
+  }, [onChange, dateState, priceState, nameState, categoryState]);
+
+  return (
+    <Container>
+      <Title>추가하기</Title>
+      <Text>날짜</Text>
+      <Textbox
+        type="date"
+        maxLength="4"
+        value={dateState}
+        onInput={(e) => setDate(e.target.value)}
+      />
+      <Text>금액</Text>
+      <PriceContainer>
+        <Textbox
+          type="text"
+          placeholder="14,000"
+          value={priceState.toLocaleString()}
+          onInput={filterNumber(setPrice)}
+        />
+        <Text>원</Text>
+      </PriceContainer>
+      <Text>이름</Text>
+      <Textbox
+        type="text"
+        placeholder="달이 장난감"
+        value={nameState}
+        onInput={(e) => setName(e.target.value)}
+      />
+      <Text>카테고리</Text>
+      <Select
+        list={categoryList}
+        outline={true}
+        value={categoryState}
+        onSelect={(item) => setCategory(item.id)}
+      />
+    </Container>
+  );
+};
+
+ExpenseModalForm.propTypes = {
+  data: PropTypes.object,
+  onChange: PropTypes.func,
+};
+
+ExpenseModalForm.defaultProps = {
+  onChange: () => {},
+};
+
+export default ExpenseModalForm;
