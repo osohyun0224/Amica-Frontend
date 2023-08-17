ededdf57c1f436af762a3adb98d6fd0471abea2e
yoona1110 <gjdbsdk18@icloud.com>
2023-08-18 04:11:19 +0900

    Design: font weight 및 여백 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -59,7 +59,7 @@ const PetName = styled.p`
   color: #151515;
   line-height: 22px;
   letter-spacing: -0.02em;
-  margin-left: 10px;
+  margin: 0 5px 0 7px;
 `;
 
 const PetRecommend = styled.div`
@@ -112,6 +112,7 @@ const DetailMenu = styled.div`
 const DetailMenuTitle = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
   margin: 0 25px 15px 0;
 `;
 
@@ -142,7 +143,7 @@ const Title = styled.div`
   font-weight: 700;
   line-height: 22px;
   letter-spacing: -0.02em;
-  margin-left: 5px;
+  margin: 0 7px;
 `;
 
 const ProductSelect = styled(Link)`
@@ -312,31 +313,34 @@ const Main = () => {
         </BannerButton>
       </BannerContainer>
       <PetRecommend>
-  <PetImage src={selectedPet ? selectedPet.image : undefined} alt={selectedPet ? selectedPet.name : "Pet Image"} />
-  <PetName>
-    {selectedPet ? `${selectedPet.name} 위해 준비했어요` : "누굴 위해 준비하니?"}
-  </PetName>
-  <PetAddBtn 
-    isDownArrow={isDropdownVisible || selectedPet}
-    src={isDropdownVisible || selectedPet ? DownArrow : AddPet} 
-    onClick={() => setDropdownVisible(!isDropdownVisible)} 
-  />
-  {isDropdownVisible && (
-    <PetDropdown>
-      {petList.map((pet) => (
-        <PetItem key={pet.id} onClick={() => {
-          console.log(pet.name);
-          setSelectedPet(pet);
-          setDropdownVisible(false);
-          setIsDropdownOpen(false);
-        }}>
-          <img src={pet.image} alt={pet.name} />
-          {pet.name}
-        </PetItem>
-      ))}
-    </PetDropdown>
-  )}
-</PetRecommend>
+        <PetImage src={selectedPet ? selectedPet.image : undefined} alt={selectedPet ? selectedPet.name : "Pet Image"} />
+        {selectedPet ?
+          <>
+            <PetName> {selectedPet.name} </PetName> 
+            위해 준비했어요 
+          </> : <PetName> 최근 등록된 상품 </PetName>
+        }  
+        <PetAddBtn 
+          isDownArrow={isDropdownVisible || selectedPet}
+          src={isDropdownVisible || selectedPet ? DownArrow : AddPet} 
+          onClick={() => setDropdownVisible(!isDropdownVisible)} 
+        />
+        {isDropdownVisible && (
+          <PetDropdown>
+            {petList.map((pet) => (
+              <PetItem key={pet.id} onClick={() => {
+                console.log(pet.name);
+                setSelectedPet(pet);
+                setDropdownVisible(false);
+                setIsDropdownOpen(false);
+              }}>
+                <img src={pet.image} alt={pet.name} />
+                {pet.name}
+              </PetItem>
+            ))}
+          </PetDropdown>
+        )}
+      </PetRecommend>
       <CategoryList>
         {Categories.map((cate) => (
           <Menu
@@ -376,7 +380,12 @@ const Main = () => {
       </DetailMenu>
       <DetailMenu>
         <DetailMenuTitle>
-          <Title> {selectedPet ? `${selectedPet.name} 위해 준비했어요` : "최근 등록된 상품"} </Title>
+          {selectedPet ?
+            <>
+              <Title> {selectedPet.name} </Title> 
+              위해 준비했어요 
+            </> : <Title> 최근 등록된 상품 </Title>
+          }  
           <NextBtn src={Arrow} alt="자세히보기" />
         </DetailMenuTitle>
         <RecommendList>
